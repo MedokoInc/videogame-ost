d536c1afb67e45a03ba2be34cb539fd4fb3bbec1
Nekonyx13 <corvin.prath@gmail.com>
2021-12-11 17:34:38 +0100

    sanitized get parameter

--- demo.php
+++ demo.php
@@ -7,11 +7,17 @@ use HTLW3R\VideoGaming\Seeder\Seeder;
 require_once 'Seeder.php';
 
 $osts = Seeder::getData();
+$availableOsts = array_keys($osts);
+$ostName = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['name']);
 
 header('Content-Type: application/json; charset=utf-8');
 
-if (isset($_GET['name'])) {
-    echo json_encode($osts[$_GET['name']]);
+if (isset($ostName)) {
+    if (in_array($ostName, $availableOsts)) {
+        echo json_encode($osts[$_GET['name']]);
+    } else {
+        echo 'OST NOT FOUND';
+    }
 }else {
     echo json_encode($osts);
 }
